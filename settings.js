540393129e55082ea450e6ed4f2ce3e8ae5d4fa1
rizkiwibu <98686896+rizkiwibu@users.noreply.github.com>
2022-06-06 12:49:57 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -18,21 +18,21 @@ global.autoread = false // auto read pesan / message
 //global.available = false //status online (online)
 
 // Other
-global.botname = "ikybot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ rizkicོ ×፝֟͜×" //nama kalian
+global.botname = "santzx bot" //namabot kalian
+global.ownername= "ᴹᴿ᭄ santzx ×፝֟͜×" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
 global.youtube = "https://youtube.com/channel/UC4nKoTls09OfD-GUTKt-6OA" //bebas asal jan hapus
 global.github = "https://github.com/rizkiwibu/" //bebas
 global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Jepang" //bebas
-global.ownernomer = "6283817161861" // nomor wa kalian
-global.ownernomerr = "6283817161861" //nmr wa kalian
+global.region = "indonesia" //bebas
+global.ownernomer = "6287735569812" // nomor wa kalian
+global.ownernomerr = "6287735569812" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
-global.owner = ["6285878836361","6285878836361","6285878836361"] //ganti agar fitur owner bisa di gunakan
-global.packname = '© Ikygakbaubawang' //sticker wm ubah
-global.author = 'Di Buat Oleh rizki' //sticker wm ganti nama kalian
+global.owner = ["6287735569812","6287735569812","6287735569812"] //ganti agar fitur owner bisa di gunakan
+global.packname = '© santzx bot' //sticker wm ubah
+global.author = 'Di Buat Oleh santzx bot' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
 global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
